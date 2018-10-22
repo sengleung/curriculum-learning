d7dd4d122dccd22656b6d4db38c448c3bd132b90
Seng Leung <sleung@tcd.ie>
2018-10-22 12:53:14 +0100

    Update fashion MNIST CNN

--- fashion-mnist-cnn/fashion-cnn.py
+++ fashion-mnist-cnn/fashion-cnn.py
@@ -10,21 +10,21 @@ from keras.layers import Dropout
 from keras.utils import to_categorical
 
 LABEL = 0
-N_TRAINING = 200
-N_TEST = 200
-# data_train = pd.read_csv('fashion-mnist_train.csv')
-TRAINING_DATA = 'fashion-mnist_test.csv'
+N_TRAINING = 10000
+N_TEST = 1000
+EPOCHS = 1
+TRAINING_DATA = 'fashion-mnist_train.csv'
 TEST_DATA = 'fashion-mnist_test.csv'
 IMG_ROWS = 28
 IMG_COLS = 28
-shape = (IMG_ROWS, IMG_COLS, 1)
+SHAPE = (IMG_ROWS, IMG_COLS, 1)
 
 
-def create_cnn(epochs=1):
+def create_cnn(x, y, epochs=EPOCHS):
     # Create model of Convolutional neural network
     classifier = Sequential()
 
-    classifier.add(Conv2D(32, (3, 3), input_shape=shape, activation='relu'))
+    classifier.add(Conv2D(32, (3, 3), input_shape=SHAPE, activation='relu'))
     classifier.add(MaxPooling2D(pool_size=(2, 2)))
     classifier.add(Dropout(0.2))
 
@@ -40,9 +40,20 @@ def create_cnn(epochs=1):
     classifier.compile(
         optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-    classifier.fit(X_train, y_train, epochs=epochs)
+    classifier.fit(x, y, epochs=epochs)
 
     return classifier
+
+
+def format_data(data):
+    x = np.array(data.iloc[:, 1:])
+    x = x.reshape(x.shape[0], IMG_ROWS, IMG_COLS, 1)
+    x = x.astype('float32')
+    x /= 255
+
+    y = to_categorical(np.array(data.iloc[:, 0]))  # training data
+
+    return x, y
 
 
 def real_classification(X_test, y_test):
@@ -55,144 +66,69 @@ def real_classification(X_test, y_test):
     return real_classes
 
 
-def test_data(data_test):
-    X_test = np.array(data_test.iloc[:, 1:])
-    X_test = X_test.reshape(X_test.shape[0], IMG_ROWS, IMG_COLS, 1)
-    X_test = X_test.astype('float32')
-    X_test /= 255
-
-    # test data     x -images y - results
-    y_test = to_categorical(np.array(data_test.iloc[:, 0]))
-
-    return X_test, y_test
+def calculate_accuracy(predicted, real):
+    correct = 0
+    for i in range(len(real)):
+        if predicted[i] == real[i]:
+            correct += 1
+    return correct / len(real)
 
 
-def test_data(data_train):
-    X_train = np.array(data_train.iloc[:, 1:])
-    X_train = X_train.reshape(X_train.shape[0], IMG_ROWS, IMG_COLS, 1)
-    X_train = X_train.astype('float32')
-    X_train /= 255
+def test_accuracy(name,  data_test, classifier):
+    # Predict training data using classifier
+    X_test, y_test = format_data(data_test)
+    predicted_classes = classifier.predict_classes(X_test)
 
-    y_train = to_categorical(np.array(data_train.iloc[:, 0]))  # training data
+    # Real classification
+    real_classes = real_classification(X_test, y_test)
 
-    return X_train, y_train
+    print(name + ':',
+          'train:', N_TRAINING,
+          'test:', N_TEST,
+          'accuracy:', calculate_accuracy(predicted_classes, real_classes))
 
 
+# Load data
 data_train = pd.read_csv(TRAINING_DATA, nrows=N_TRAINING)
-data_test = pd.read_csv(TEST_DATA,  nrows=N_TEST)
+data_test = pd.read_csv(TEST_DATA, nrows=N_TEST)
+curriculum_data = pd.read_csv(TRAINING_DATA).tail(N_TRAINING)
+
+# Create training model without curriculum and calculate accuracy
+X_train, y_train = format_data(data_train)
+classifier = create_cnn(X_train, y_train)
+test_accuracy('No curriculum', data_test, classifier)
 
 
-# Test data
-
-X_train, y_train = test_data(data_train)
-
-X_test, y_test = test_data(data_test)
-
-classifier = create_cnn(1)
-
-# Predict training data
-
+# Classify latter half of data to group data into easy and hard
+X_test, y_test = format_data(curriculum_data)
 predicted_classes = classifier.predict_classes(X_test)
-
-# Gather testing data real classification
-
 real_classes = real_classification(X_test, y_test)
-
-# Compare predicted classification with real classification
 
 correct = 0
 incorrects = set()
 for x in range(len(X_train)):
-    if(predicted_classes[x] == real_classes[x]):
+    if predicted_classes[x] == real_classes[x]:
         correct += 1
     else:
         incorrects.add(x)
 
-print('The accuracy is', correct / len(X_test))
+print('Classifed curriculum', correct / len(X_test))
 
-data_test_list = data_train.values
+# Sort data with easy first and hard next
+curriculum_data_list = curriculum_data.values
 
+easy = []
+hard = []
+for i in range(len(X_train)):
+    if i not in incorrects:
+        easy.append(curriculum_data_list[i])
+    else:
+        hard.append(curriculum_data_list[i])
 
-# easy = []
-# hard = []
-# for i in range(len(X_train)):
-#     if i not in incorrects:
-#         easy.append(data_test_list[i])
-#     else:
-#         hard.append(data_test_list[i])
-#
-# easy_data = pd.DataFrame(easy)
-# hard_data = pd.DataFrame(hard)
+combined = easy + hard
+curriculum = pd.DataFrame(combined)
 
-# ------------------------------------------------------
-
-# data_test = pd.read_csv('fashion-mnist_test.csv',  nrows=N_TEST)
-# X_train = np.array(easy_data.iloc[:, 1:])
-# y_train = to_categorical(np.array(easy_data.iloc[:, 0]))  # training data
-# # X_test = np.array(data_test.iloc[:, 1:])
-# # y_test = to_categorical(np.array(data_test.iloc[:, 0]))    # test data
-#
-# X_train = X_train.reshape(X_train.shape[0], IMG_ROWS, IMG_COLS, 1)
-# X_test = X_test.reshape(X_test.shape[0], IMG_ROWS, IMG_COLS, 1)
-#
-# X_train = X_train.astype('float32')
-# X_test = X_test.astype('float32')
-# X_train /= 255
-# X_test /= 255
-#
-# classifier = Sequential()
-#
-# classifier.add(Conv2D(32, (3, 3), input_shape=shape, activation='relu'))
-# classifier.add(MaxPooling2D(pool_size=(2, 2)))
-# classifier.add(Dropout(0.2))
-#
-# classifier.add(Conv2D(32, (3, 3), activation='relu'))
-# classifier.add(MaxPooling2D(pool_size=(2, 2)))
-# classifier.add(Dropout(0.4))
-#
-# classifier.add(Flatten())
-#
-# classifier.add(Dense(units=128, activation='relu'))
-# classifier.add(Dense(units=10, activation='sigmoid'))
-#
-# classifier.compile(
-#     optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-#
-# classifier.fit(X_train, y_train, epochs=5)
-#
-# correct = 0
-# incorrects = set()
-# for x in range(len(X_test)):
-#     if(predicted_classes[x] == real_classes[x]):
-#         correct += 1
-#     else:
-#         incorrects.add(x)
-#
-# print('The accuracy is', correct / len(X_test))
-# data_test = pd.read_csv('fashion-mnist_test.csv',  nrows=N_TEST)
-# X_train = np.array(hard_data.iloc[:, 1:])
-# y_train = to_categorical(np.array(hard_data.iloc[:, 0]))  # training data
-# X_test = np.array(data_test.iloc[:, 1:])
-# y_test = to_categorical(np.array(data_test.iloc[:, 0]))    # test data
-#
-# X_train = X_train.reshape(X_train.shape[0], IMG_ROWS, IMG_COLS, 1)
-# X_test = X_test.reshape(X_test.shape[0], IMG_ROWS, IMG_COLS, 1)
-#
-# X_train = X_train.astype('float32')
-# X_test = X_test.astype('float32')
-# X_train /= 255
-# X_test /= 255
-#
-# predicted_classes = classifier.predict_classes(X_test)
-#
-# classifier.fit(X_train, y_train, epochs=5)
-#
-# correct = 0
-# incorrects = set()
-# for x in range(len(X_test)):
-#     if(predicted_classes[x] == real_classes[x]):
-#         correct += 1
-#     else:
-#         incorrects.add(x)
-#
-# print('The accuracy is', correct / len(X_test))
+# Create training model with curriculum and calculate accuracy
+X_train, y_train = format_data(curriculum)
+classifier2 = create_cnn(X_train, y_train)
+test_accuracy('With curriculum', data_test, classifier2)
