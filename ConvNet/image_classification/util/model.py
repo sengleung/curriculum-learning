3c9afbcfd5a7fbef70e7e22a9d31a8956fede8c0
Eddie Bergman <eddiebergmanhs@gmail.com>
2018-12-16 17:16:11 +0000

    Created model configs to obtain results for

--- ConvNet/image_classification/util/model.py
+++ ConvNet/image_classification/util/model.py
@@ -3,10 +3,8 @@ from keras.models import Sequential, load_model
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 
-filepath = './models/'
+def load(name, filepath):
+    return load_model(filepath + name + '.h5')
 
-def load(name, id=0):
-    return load_model(filepath + name + '_' + str(id) + '.h5')
-
-def save(model, name, id=0):
-    model.save(filepath + name + '_' + str(id) + '.h5')
+def save(model, filepath,  name):
+    model.save(filepath + '/' + name + '.h5')
