54cf130fd620afb385a8ea4ef0bd7ffedf0b1efa
Eddie Bergman <eddiebergmanhs@gmail.com>
2018-12-17 02:29:42 +0000

    last fix?

--- setup_step1.sh
+++ setup_step1.sh
@@ -1,8 +1,5 @@
-cd ~
-mkdir curriculum-learning/ConvNet/image_classification/datasets
-mv emnist_balanced.zip curriculum-learning/ConvNet/image_classification/datasets/emnist_balanced.zip
+mkdir ~/curriculum-learning/ConvNet/image_classification/datasets
+mv ~/emnist_balanced.zip ~/curriculum-learning/ConvNet/image_classification/datasets/emnist_balanced.zip
 sudo apt-get install python-virtualenv unzip -y
-cd ~/curriculum-learning/ConvNet/image_classification/datasets
-unzip emnist_balanced.zip
-cd ~
+unzip ~/curriculum-learning/ConvNet/image_classification/emnist_balanced.zip
 virtualenv --python=python3.5 python3.5venv
