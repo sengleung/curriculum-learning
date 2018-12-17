0671c74e29ffc826cb82bdd929b6586fc3128339
Eddie Bergman <eddiebergmanhs@gmail.com>
2018-12-17 02:23:11 +0000

    Fixed setup_step1 and auto-confirmed argument

--- setup_step1.sh
+++ setup_step1.sh
@@ -1,9 +1,8 @@
 cd ~
 mkdir curriculum-learning/ConvNet/image_classification/datasets
 mv emnist_balanced.zip curriculum-learning/ConvNet/image_classification/datasets/emnist_balanced.zip
-sudo apt-get install python-virtualenv unzip
+sudo apt-get install python-virtualenv unzip -y
 cd curriculum-learning/ConvNet/image_classification/datasets
 unzip emnist_balanced.zip
 cd ~
 virtualenv --python=python3.5 python3.5venv
-source ./python3.5venv/bin/activate
