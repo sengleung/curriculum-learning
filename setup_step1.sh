2cf3f0253a25d21efbe458b075c753ab5c86f1b7
Eddie Bergman <eddiebergmanhs@gmail.com>
2018-12-17 02:51:23 +0000

    How many errors can one make in a 5 line file?

--- setup_step1.sh
+++ setup_step1.sh
@@ -1,5 +1,5 @@
 mkdir ~/curriculum-learning/ConvNet/image_classification/datasets
 mv ~/emnist_balanced.zip ~/curriculum-learning/ConvNet/image_classification/datasets/emnist_balanced.zip
 sudo apt-get install python-virtualenv unzip -y
-unzip ~/curriculum-learning/ConvNet/image_classification/emnist_balanced.zip
+unzip ~/curriculum-learning/ConvNet/image_classification/datasets/emnist_balanced.zip -d ~/curriculum-learning/ConvNet/image_classification/datasets/
 virtualenv --python=python3.5 ~/python3.5venv
